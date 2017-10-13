3b065829dfcec21d3da53919dc8b36f7897057f2
srilekha <srilekha@srilekha-HP-Pavilion-g6-Notebook-PC>
2017-10-13 09:59:07 +0530

    My first hibernate project

---
+++ hiberproj/src/main/java/com/Advajava/hiberproj/Alien.java
@@ -0,0 +1,27 @@
+package com.Advajava.hiberproj;
+
+public class Alien {
+	private String name;
+	private String tech;
+	private int Age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	
+
+}
---
+++ hiberproj/src/main/java/com/Advajava/hiberproj/App.java
@@ -0,0 +1,16 @@
+package com.Advajava.hiberproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Alien obj=new Alien();
+        obj.setAge(21);
+        obj.setName("sri");
+        obj.setTech("c,java");
+    }
+}
